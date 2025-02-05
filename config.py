3dbf33ea6e7f976cc7e156f12db2e61728940aa3
cordz-del <raarongraham@gmail.com>
2025-02-05 12:42:12 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME", "r.aarongraham.wv@gmail.com")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD", "El!jah0327")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
