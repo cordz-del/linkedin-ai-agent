552fc080f9d7efe5ceb234d83e50b61213b69634
cordz-del <raarongraham@gmail.com>
2025-02-06 05:26:11 -0800

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,14 @@
 import os
 import schedule
 import time
-from flask import Flask
 from automation import LinkedInAutomation
 
-app = Flask(__name__)
 automation = LinkedInAutomation()
 
 schedule.every().day.at("08:00").do(automation.post_on_linkedin)
 schedule.every().day.at("23:00").do(automation.post_on_linkedin)
-@app.route('/')
-def run_scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
-    return "Scheduler is running"
 
+while True:
+    schedule.run_pending()
+    time.sleep(60)
 
-pp.run(host="0.0.0.0", port=os.environ.get('PORT', 8080))
